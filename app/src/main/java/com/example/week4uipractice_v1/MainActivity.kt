668416ad18be4b0d39a05e0c2dbef06a2209512b
Sim981 <nepp.plus@gmail.com>
2022-05-06 18:06:33 +0900

    로그인 로직 완성

--- app/src/main/java/com/example/week4uipractice_v1/MainActivity.kt
+++ app/src/main/java/com/example/week4uipractice_v1/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.week4uipractice_v1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logoutBtn.setOnClickListener {
+            val myIntent = Intent(this, LoginActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }
     }
 }
